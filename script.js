c9c48d1865a98519afdacb7c1ef63ddce0435ef5
yash chuhan <curlmanofficial@gmail.com>
2025-05-19 00:14:55 +0530

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Initialize Lenis
+//  for scroll smoothly
 const lenis = new Lenis({
     duration: 1.2,
     smooth: true,
 });
 
-// Use requestAnimationFrame to continuously update the scroll
+//  for scroll smoothly
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
@@ -48,5 +48,50 @@ document.querySelectorAll(".elem").forEach((elem) => {
         })
          })
 
-         // Quiz functionality
+         // for horizontal scrolling section
 
+gsap.registerPlugin(ScrollTrigger);
+
+const contents = gsap.utils.toArray("#horizontal .content");
+    gsap.to(contents, {
+         xPercent : -100 * (contents.length - 1),
+         scrollTrigger : {
+            trigger : "#horizontal",
+            pin : true,
+            scrub : 1,
+            snap: 1 / (contents.length - 1),
+            end: "+=3000",
+           
+         }
+
+        });
+        // for Thank you! section
+  
+        var tl = gsap.timeline({scrollTrigger: {
+         trigger: "#chacha",
+         markers: false,
+            start: "50% 50%",
+            end: "100% 50%",
+            scrub: 2,
+            pin : true,               
+         }});
+         tl.to("#top", {
+            top: "-50%",
+         }, 'a').to("#btm", {
+            top: "60%",
+         }, 'a')
+         .to("#mmm", {
+            marginTop: "0%", 
+         })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = [
+        {id :"#card1" , endTranslateX: -2000, rotate:45},
+        {id :"#card2" , endTranslateX: -1000, rotate:-30},
+        {id :"#card3" , endTranslateX: -2000, rotate:45},
+        {id :"#card3" , endTranslateX: -1500, rotate:-30},
+    ]
+
+          
+})
